47119e91a55d623e7e2cbce686b8d60f0fad9a58
jaturapakk <pak3535story@gmail.com>
2024-01-01 22:57:23 +0700

    add route path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,10 @@ import CustomerRegister from './pages/customerregister';
 import HomePage from './pages/homepage';
 import ReviewPage from './pages/profile/review';
 import Admin from './pages/admin';
-
+import EditProfilePage from './pages/EditProfilePage/EditProfilePage';
+import CartPage from './pages/CartPage/CartPage';
+import WishlistPage from './pages/WishlistPage/WishlistPage';
+import PurchasePage from './pages/PurchasePage/PurchasePage';
 import './App.css';
 
 function App() {
@@ -12,9 +15,13 @@ function App() {
     <Router>
       <Routes>
         <Route path='/register' Component={CustomerRegister}/>
-        <Route path='/home' Component={HomePage}/>
+        <Route path='/' Component={HomePage}/>
         <Route path='/review' Component={ReviewPage}/>
         <Route path='/admin' Component={Admin}/>
+        <Route path="/EditProfilePage"      element={<EditProfilePage />} />
+        <Route path="/CartPage"      element={<CartPage />} />
+        <Route path="/WishlistPage"      element={<WishlistPage />} />
+        <Route path="/PurchasePage"      element={<PurchasePage />} />
       </Routes>
     </Router>
     </>
